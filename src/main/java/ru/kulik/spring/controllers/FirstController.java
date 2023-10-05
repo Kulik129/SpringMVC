5f2ab6d24164d4afa33a66bd583d733a3fdd71fb
mitya.kulikbk.ru <mitya.kulik@bk.ru>
2023-10-05 21:38:42 +0300

    Работа с контроллерами и анотацией @RequestParam

--- src/main/java/ru/kulik/spring/controllers/FirstController.java
+++ src/main/java/ru/kulik/spring/controllers/FirstController.java
@@ -2,6 +2,7 @@ package ru.kulik.spring.controllers;
 
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -11,14 +12,31 @@ import org.springframework.web.bind.annotation.RequestParam;
 public class FirstController {
     @GetMapping("/hello")
     public String helloPage(@RequestParam(value = "name", required = false) String name,
-                            @RequestParam(value = "surname", required = false) String surname) {
-
-        System.out.println("Имя " + name + " Фамилия " + surname);
+                            @RequestParam(value = "surname", required = false) String surname,
+                            Model model) {
+        model.addAttribute("message", "Name " + name + " Surname " + surname);
         return "first/hello";
     }
 
     @GetMapping("/good-bye")
     public String goodByePage() {
         return "first/goodBye";
+    }
+
+    @GetMapping("/calculator")
+    public String calculator(@RequestParam("action") String action,
+                             @RequestParam("a") int a,
+                             @RequestParam("b")int b,
+                             Model model){
+        switch (action){
+            case "*":
+                model.addAttribute("ans", a*b);
+                break;
+            case "pl":
+                model.addAttribute("ans", a+b);
+                break;
+        }
+
+        return "first/ans";
     }
 }
